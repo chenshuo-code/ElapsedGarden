9e8822a5c6a8156b5c50d71482817ffd3d1fb17a
chenshuo-code <lichenshuome@hotmail.com>
2022-07-19 10:38:08 +0200

    Update new function of lotus, and test function of plant affect

--- Assets/Scripts/SceneObjects/Plant_Lotus.cs
+++ Assets/Scripts/SceneObjects/Plant_Lotus.cs
@@ -4,9 +4,62 @@ using UnityEngine;
 
 public class Plant_Lotus : MonoBehaviour
 {
-    public bool isFirstLotus;
+    /// <summary>
+    /// Time delay to show lotus, in secound (>1)
+    /// </summary>
+    public float ShowWaitTime;
+
+    private TriggerLotus triggerLotus;
+    private SkinnedMeshRenderer lotusMesh;
+    private bool isLotusActive=false;
+
+    private void Start()
+    {
+        triggerLotus =  transform.GetComponentInChildren<TriggerLotus>();
+        lotusMesh = transform.GetComponentInChildren<SkinnedMeshRenderer>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Fish")&&isLotusActive)
+        {
+            ShowLotus();
+        }
+        if (other.CompareTag("Player"))
+        {
+            ActivePlate();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Fish")&&isLotusActive)
+        {
+            HideLotus();
+        }
+    }
+    private void ShowLotus()
+    {
+        //triggerLotus.gameObject.SetActive(true);
+        lotusMesh.gameObject.SetActive(true);
+    }
+    private void HideLotus()
+    {
+        //triggerLotus.gameObject.SetActive(false);
+        lotusMesh.gameObject.SetActive(false);
+    }
+    private void ActivePlate()
+    {
+        print("我活啦");
+        isLotusActive = false;
+        Invoke("DeactivatePlate",5f);
+    }
+    private void DeactivatePlate()
+    {
+        print("我又死了");
+        HideLotus();
+        isLotusActive = false;
+    }
     public void ActiveLotus()
     {
-
+        isLotusActive = true;
     }
 }
---
+++ Assets/Scripts/SceneObjects/TriggerLotus.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerLotus : MonoBehaviour
+{
+    private Plant_Lotus lotus;
+    private void Start()
+    {
+        transform.GetComponentInParent<Plant_Lotus>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            lotus.ActiveLotus();
+        }
+    }
+}
